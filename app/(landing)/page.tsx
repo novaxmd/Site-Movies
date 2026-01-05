ac45276df1a00e12d0fd8c2a7168698b4a22b775
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2026-01-06 01:47:26 +0300

    Create page.tsx

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,47 @@
+import React, { Suspense } from 'react'
+import { Metadata } from 'next'
+import { populateHomePageData } from '@/services/movies'
+
+import { siteConfig } from '@/config/site'
+import { HeroSlider } from '@/components/header/hero-slider'
+import { FullScreenLoader } from '@/components/loaders/intro-pages-loader'
+import { MoviesIntroSection } from '@/components/main-page/intro-section'
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  // Open Graph and Twitter images will be automatically handled by
+  // opengraph-image.png and twitter-image.png files in the app directory
+}
+
+async function IndexPage() {
+  const {
+    trendingMediaForHero,
+    latestTrendingMovies,
+    allTimeTopRatedMovies,
+    popularMovies,
+    latestTrendingSeries,
+    popularSeries,
+    allTimeTopRatedSeries,
+  } = await populateHomePageData()
+  return (
+    <section className="h-full">
+      <Suspense fallback={<FullScreenLoader />}>
+        <HeroSlider movies={trendingMediaForHero} />
+      </Suspense>
+      <MoviesIntroSection
+        latestTrendingMovies={latestTrendingMovies}
+        allTimeTopRatedMovies={allTimeTopRatedMovies}
+        popularMovies={popularMovies}
+        latestTrendingSeries={latestTrendingSeries}
+        popularSeries={popularSeries}
+        allTimeTopRatedSeries={allTimeTopRatedSeries}
+      />
+    </section>
+  )
+}
+export default IndexPage
+    
