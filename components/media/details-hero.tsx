e858c385de8433732dc121ca2b9e7e0247db04c4
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2026-01-06 02:19:02 +0300

    Add files via upload

---
+++ components/media/details-hero.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React from 'react'
+
+import { MovieDetails } from '@/types/movie-details'
+import { STREAMING_MOVIES_API_URL } from '@/lib/constants'
+import { DetailsHero } from '@/components/details-hero'
+
+export const MovieDetailsHero = ({ movie }: { movie: MovieDetails }) => {
+  const [isIframeShown, setIsIframeShown] = React.useState(false)
+  const iframeRef = React.useRef<HTMLIFrameElement>(null)
+
+  const playVideo = () => {
+    if (iframeRef.current) {
+      setIsIframeShown(true)
+      iframeRef.current.src = `${STREAMING_MOVIES_API_URL}/movie/${movie?.id}`
+    }
+  }
+  return (
+    <DetailsHero
+      movie={movie}
+      isIframeShown={isIframeShown}
+      playVideo={playVideo}
+      ref={iframeRef}
+    />
+  )
+}
